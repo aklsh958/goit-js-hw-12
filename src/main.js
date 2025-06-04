e1a5038519a866f857e13acb9a747431b556c363
Artem Kulish <akulish247@gmail.com>
2025-06-04 13:08:25 +0200

    update

--- src/main.js
+++ src/main.js
@@ -16,7 +16,7 @@ const input = document.querySelector('input[name="search-text"]');
 const loadBtn = document.querySelector('.load-btn');
 
 let query = '';
-let page = 0;
+let page = 1; 
 const perPage = 15;
 let totalHits = 0;
 
@@ -38,25 +38,22 @@ form.addEventListener('submit', async e => {
   try {
     const data = await getImagesByQuery(query, page, perPage);
     totalHits = data.totalHits;
-    // totalHits = 100;
 
-    setTimeout(() => {
-      hideLoader();
+    hideLoader(); 
 
-      if (data.hits.length === 0) {
-        iziToast.error({
-          id: 'error',
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-          position: 'topRight',
-          transitionIn: 'fadeInDown',
-        });
-        return;
-      }
+    if (data.hits.length === 0) {
+      iziToast.error({
+        id: 'error',
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+        position: 'topRight',
+        transitionIn: 'fadeInDown',
+      });
+      return;
+    }
 
-      createGallery(data.hits);
-      if (page * perPage < totalHits) showLoadMoreButton();
-    }, 600);
+    createGallery(data.hits);
+    if (page * perPage < totalHits) showLoadMoreButton();
   } catch (error) {
     hideLoader();
     iziToast.error({
@@ -85,22 +82,20 @@ loadBtn.addEventListener('click', async () => {
 
   try {
     const data = await getImagesByQuery(query, page, perPage);
-    setTimeout(() => {
-      hideLoader();
-      createGallery(data.hits);
-      smoothScroll();
-      //   console.log(data.hits.map(img => img.id));
 
-      if (page * perPage >= totalHits) {
-        iziToast.info({
-          message: "We're sorry, but you've reached the end of search results.",
-          position: 'bottomRight',
-        });
-        hideLoadMoreButton();
-      } else {
-        showLoadMoreButton();
-      }
-    }, 600);
+    hideLoader(); 
+    createGallery(data.hits);
+    smoothScroll();
+
+    if (page * perPage >= totalHits) {
+      iziToast.info({
+        message: "We're sorry, but you've reached the end of search results.",
+        position: 'bottomRight',
+      });
+      hideLoadMoreButton();
+    } else {
+      showLoadMoreButton();
+    }
   } catch (error) {
     hideLoader();
     iziToast.error({
